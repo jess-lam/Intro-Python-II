afa323083503f8fea2c3c48fb242724db8d033e6
Jessica Lam <jemlam93@gmail.com>
2020-09-09 12:40:16 -0700

    reformatted it so that I can access name and description attributes to print for current room

--- src/adv.py
+++ src/adv.py
@@ -41,7 +41,11 @@ room['treasure'].s_to = room['narrow']
 # Make a new player object that is currently in the 'outside' room.
 
 new_player = Player("Jess", room["outside"])
-print(new_player)
+print(f" {new_player.name} is in {new_player.current_room.name}")
+#print(new_player.current_room.description)
+
+#$print(room[new_player.current_room].description)
+
 
 #print(new_player)
 # print(room.items())
@@ -50,10 +54,23 @@ print(new_player)
 # print(room.get("outside").description)
 
 # Write a loop that:
+"""
+for r in room:
+    print(room[r].name)
+    print(room[r].description)
+"""
+
 #
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
+while True:
+    print(f"\n Currently in room: {new_player.current_room.name}")
+    print(f"\n Room Description: {new_player.current_room.description}")
+
+    """
+    Now need to implement input
+    """
 #
 # If the user enters a cardinal direction, attempt to move to the room there.
 # Print an error message if the movement isn't allowed.
--- src/player.py
+++ src/player.py
@@ -5,8 +5,9 @@ class Player:
     def __init__(self, name, current_room):
         self.name = name
         self.current_room = current_room
-
+"""
     def __str__(self):
-        return f"{self.name} is in {self.current_room.name}"
+        return f"{self.name} is in {self.current_room}"
+"""
 
 
